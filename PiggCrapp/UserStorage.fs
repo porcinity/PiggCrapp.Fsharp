38c77014791d80861f9cb505c1dd7c6317f2fd49
Ant <it@me.org>
2022-01-21 17:18:36 -0600

    Add UserStorage.fs

---
+++ PiggCrapp/UserStorage.fs
@@ -0,0 +1,38 @@
+module PiggCrapp.UserStorage
+
+open Npgsql.FSharp
+open WorkoutModel
+
+let connStr = "Host=localhost;Database=PiggCrapp;Username=test;Password=test"
+
+let findUsersAsync =
+    connStr
+    |> Sql.connect
+    |> Sql.query "select * from users"
+    |> Sql.executeAsync (fun read ->
+        {
+            UserId = read.uuid "user_id" |> UserId
+            Name = read.text "user_name" |> UserName
+        })
+    
+let findUserAsync userId =
+    connStr
+    |> Sql.connect
+    |> Sql.query "select * from users where user_id = @id"
+    |> Sql.parameters [ "@id", Sql.uuid userId ]
+    |> Sql.executeAsync (fun read ->
+        {
+            UserId = read.uuid "user_id" |> UserId
+            Name = read.text "user_name" |> UserName
+        })
+    
+let insertUserAsync user =
+    connStr
+    |> Sql.connect
+    |> Sql.query "insert into users (user_id, user_name)
+                  values (@id, @name)"
+    |> Sql.parameters [
+        "@id", Sql.uuid <| UserId.toGuid user.UserId
+        "@name", Sql.text <| UserName.toString user.Name
+    ]
+    |> Sql.executeNonQueryAsync
