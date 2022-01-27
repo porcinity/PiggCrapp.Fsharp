9f33c98545d808a6054789b7c17105126405b6fb
Ant <it@me.org>
2022-01-27 08:31:35 -0600

    Update UserStorage functions to use Age and Weight

--- PiggCrapp/UserStorage.fs
+++ PiggCrapp/UserStorage.fs
@@ -1,10 +1,10 @@
 module PiggCrapp.UserStorage
 
 open Npgsql.FSharp
-open WorkoutModel
+open PiggCrapp.Users
+open PiggCrapp.Measurements
 
 let connStr = "Host=localhost;Database=PiggCrapp;Username=pigg"
-
 
 let findUsersAsync () =
     connStr
@@ -14,7 +14,9 @@ let findUsersAsync () =
         {
             UserId = read.uuid "user_id" |> UserId
             Name = read.text "user_name" |> UserName
-            Weight = read.double "user_weight" * 1.0<lbs> |> UserWeight 
+            Age = read.int "user_age" |> UserAge
+            Weight = read.double "user_weight" * 1.0<lbs> |> UserWeight
+            CreatedDate = read.dateTime "created_date"
         })
     
 let findUserAsync userId =
@@ -26,18 +28,22 @@ let findUserAsync userId =
         {
             UserId = read.uuid "user_id" |> UserId
             Name = read.text "user_name" |> UserName
+            Age = read.int "user_age" |> UserAge
             Weight = read.double "user_weight" * 1.0<lbs> |> UserWeight
+            CreatedDate = read.dateTime "created_date"
         })
     
 let insertUserAsync user =
     connStr
     |> Sql.connect
-    |> Sql.query "insert into users (user_id, user_name, user_weight)
-                  values (@id, @name, @weight)"
+    |> Sql.query "insert into users (user_id, user_name, user_age, user_weight, created_date)
+                  values (@id, @name, @age, @weight, @date)"
     |> Sql.parameters [
         "@id", Sql.uuid <| UserId.toGuid user.UserId
         "@name", Sql.text <| UserName.toString user.Name
+        "@age", Sql.int <| UserAge.toInt user.Age
         "@weight", Sql.double <| UserWeight.toFloat user.Weight
+        "@date", Sql.timestamp <| user.CreatedDate
     ]
     |> Sql.executeNonQueryAsync
     
