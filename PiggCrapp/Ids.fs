5b4d18cb23d6fefcf0bd749357d78027ca5af067
Ant <it@me.org>
2022-01-27 09:20:06 -0600

    Collate Ids into Ids.fs

---
+++ PiggCrapp/Ids.fs
@@ -0,0 +1,30 @@
+module PiggCrapp.Ids
+
+open System
+
+type ExerciseId = ExerciseId of Guid
+
+module ExerciseId =
+    let toGuid (ExerciseId guid) = guid
+
+type WorkoutId = WorkoutId of Guid
+
+module WorkoutId =
+    let toGuid (WorkoutId guid) = guid
+
+type UserId = UserId of Guid
+
+module UserId =
+    let toGuid (UserId guid) = guid
+    
+//type Id =
+//    | ExerciseId of Guid
+//    | WorkoutId of Guid
+//    | UserId of Guid
+    
+//module Id =
+//    let unwrap id =
+//        match id with
+//        | ExerciseId id -> id
+//        | WorkoutId id -> id
+//        | UserId id -> id
