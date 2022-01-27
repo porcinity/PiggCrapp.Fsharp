6a7061e3f83aacf621e2876fa95ac0e5f6eb8a79
Ant <it@me.org>
2022-01-26 18:13:06 -0600

    Add main Program file for Api

---
+++ PiggCrapp.Api/Program.fs
@@ -0,0 +1,40 @@
+module PiggCrapp.Api.Program
+
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.Extensions.Hosting
+open Microsoft.Extensions.DependencyInjection
+open Giraffe
+
+let webApp =
+    choose [
+        GET >=>
+            choose [
+                route "/users" >=> getUsers        
+            ]
+        POST >=>
+            choose [
+                route "/users" >=> postUser
+            ]
+        ]
+
+let configureApp (app : IApplicationBuilder) =
+    // Add Giraffe to the ASP.NET Core pipeline
+    app.UseGiraffe webApp
+
+let configureServices (services : IServiceCollection) =
+    // Add Giraffe dependencies
+    services.AddGiraffe() |> ignore
+
+[<EntryPoint>]
+let main _ =
+    Host.CreateDefaultBuilder()
+        .ConfigureWebHostDefaults(
+            fun webHostBuilder ->
+                webHostBuilder
+                    .Configure(configureApp)
+                    .ConfigureServices(configureServices)
+                    |> ignore)
+        .Build()
+        .Run()
+    0
