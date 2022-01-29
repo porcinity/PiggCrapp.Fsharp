9d7ca6fc5282da685b9f87440337ae9f47b91d50
Ant <it@me.org>
2022-01-29 13:43:06 -0600

    Update SetStorage.fs

--- PiggCrapp.Domain/SetStorage.fs
+++ PiggCrapp.Domain/SetStorage.fs
@@ -1,50 +1,76 @@
-module PiggCrapp.SetStorage
+module PiggCrapp.Domain.SetStorage
 
 open Npgsql.FSharp
 open PiggCrapp.Domain.Ids
 open PiggCrapp.Domain.Sets
 
 
-let connStr = "Host=localhost;Database=PiggCrapp;Username=test;Password=test"
+let connStr = "Host=localhost;Database=PiggCrapp;Username=pigg"
 
 let findSetsAsync exerciseId =
     connStr
     |> Sql.connect
-    |> Sql.query "select * from sets
-                  from exercises e
-                  join sets s on e.exercise_id = s.exercise_id
-                  where s.exercise_id = @id"
+    |> Sql.query "select *
+                  from sets
+                  where exercise_id = @id"
     |> Sql.parameters [ "@id", Sql.uuid <| ExerciseId.toGuid exerciseId ]
-    |> Sql.execute (fun read ->
+    |> Sql.executeAsync (fun read ->
         {
-           RegularSetId = read.uuid "set_id" |> RegularSetId
-           Weight = read.double "weight" |> Weight
-           Reps = read.int "reps" |> Reps
+           RegularSetId = read.int "set_id" |> RegularSetId
+           Weight = read.double "set_weight" |> Weight
+           Reps = read.int "set_reps" |> Reps
            Exercise = read.uuid "exercise_id" |> ExerciseId
         })
     
-let findSetAsync setId =
+let findSetAsync exerciseId setId =
     connStr
     |> Sql.connect
-    |> Sql.query "select * from sets where set_id = @id"
-    |> Sql.parameters [ "@id", Sql.uuid <| RegularSetId.toGuid setId ]
-    |> Sql.execute (fun read ->
+    |> Sql.query "select * from sets where set_id = @id and exercise_id = @ex"
+    |> Sql.parameters [
+        "@id", Sql.int <| RegularSetId.toInt setId
+        "@ex", Sql.uuid <| ExerciseId.toGuid exerciseId
+    ]
+    |> Sql.executeAsync (fun read ->
         {
-           RegularSetId = read.uuid "set_id" |> RegularSetId
-           Weight = read.double "weight" |> Weight
-           Reps = read.int "reps" |> Reps
+           RegularSetId = read.int "set_id" |> RegularSetId
+           Weight = read.double "set_weight" |> Weight
+           Reps = read.int "set_reps" |> Reps
            Exercise = read.uuid "exercise_id" |> ExerciseId
         })
     
 let insertSetAsync set =
     connStr
     |> Sql.connect
-    |> Sql.query "insert into sets (set_id, weight, reps, exercise_id)
+    |> Sql.query "insert into sets (set_id, set_weight, set_reps, exercise_id)
                   values (@id, @weight, @reps, @exercise_id)"
     |> Sql.parameters [
-        "@id", Sql.uuid <| RegularSetId.toGuid set.RegularSetId
+        "@id", Sql.int <| RegularSetId.toInt set.RegularSetId
         "@weight", Sql.double <| Weight.toDouble set.Weight
         "@reps", Sql.int <| Reps.toInt set.Reps
         "@exercise_id", Sql.uuid <| ExerciseId.toGuid set.Exercise
+    ]
+    |> Sql.executeNonQueryAsync
+
+let updateSetAsync set =
+    connStr
+    |> Sql.connect
+    |> Sql.query "update sets
+                  set set_weight = @weight, set_reps = @reps
+                  where set_id = @setId and exercise_id = @exerciseId"
+    |> Sql.parameters [
+        "@setId", Sql.int <| RegularSetId.toInt set.RegularSetId
+        "@weight", Sql.double <| Weight.toDouble set.Weight
+        "@reps", Sql.int <| Reps.toInt set.Reps
+        "@exerciseId", Sql.uuid <| ExerciseId.toGuid set.Exercise
+    ]
+    |> Sql.executeNonQueryAsync
+
+let deleteSetAsync exerciseId setId =
+    connStr
+    |> Sql.connect
+    |> Sql.query "delete from sets where set_id = @setId and exercise_id = @exerciseId"
+    |> Sql.parameters [
+        "@setId", Sql.int <| RegularSetId.toInt setId
+        "@exerciseId", Sql.uuid <| ExerciseId.toGuid exerciseId
     ]
     |> Sql.executeNonQueryAsync
