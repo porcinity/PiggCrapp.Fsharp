2f900f64123e62191b2ffc45901afc881666b0d3
Ant <it@me.org>
2022-01-31 15:38:38 -0600

    Auto format Exercises.fs

--- PiggCrapp.Domain/Exercises.fs
+++ PiggCrapp.Domain/Exercises.fs
@@ -8,31 +8,32 @@ open PiggCrapp.Domain.ExerciseNotes
 
 type ExerciseName = ExerciseName of string
 
-module ExerciseName =    
+module ExerciseName =
     let (|Empty|TooLong|SpecialChars|ValidExerciseName|) (str: string) =
         match str with
         | "" -> Empty
-        | x when String.length(x) > 50 -> TooLong
+        | x when String.length (x) > 50 -> TooLong
         | x when Regex.IsMatch(x, "^[a-zA-Z][a-zA-Z\s]*$") = false -> SpecialChars
         | _ -> ValidExerciseName
 
     let create (name: string) =
         let trim = name.TrimStart(' ').TrimEnd(' ')
+
         match trim with
         | Empty -> Error [ "Exercise name can't be blank." ]
         | TooLong -> Error [ "Exercise name can't be more than 50 characters." ]
         | SpecialChars -> Error [ "Exercise name can't contain numbers or special chars." ]
         | ValidExerciseName -> ExerciseName trim |> Ok
-        
+
     let toString (ExerciseName name) = name
-        
+
 type Exercise =
-    { ExerciseId : ExerciseId
-      Name : ExerciseName
-      Notes : ExerciseNotes option
-      Sets : Set list
-      WorkoutId : WorkoutId }
-    
+    { ExerciseId: ExerciseId
+      Name: ExerciseName
+      Notes: ExerciseNotes option
+      Sets: Set list
+      WorkoutId: WorkoutId }
+
 module Exercise =
     let create exerciseName workoutId =
         { ExerciseId = ExerciseId <| Guid.NewGuid()
@@ -40,15 +41,15 @@ module Exercise =
           Notes = None
           Sets = []
           WorkoutId = workoutId }
-        
-    let changeName (exercise: Exercise) name =
-        { exercise with Name = name }
-    
+
+    let changeName (exercise: Exercise) name = { exercise with Name = name }
+
     let addSet exercise set =
-        { exercise with Sets = exercise.Sets @ [set] }
-        
+        { exercise with
+              Sets = exercise.Sets @ [ set ] }
+
     let removeSet exercise set =
         let setsWithout =
-            exercise.Sets
-            |> List.filter (fun x -> x <> set)
+            exercise.Sets |> List.filter (fun x -> x <> set)
+
         { exercise with Sets = setsWithout }
