32fa6548f3e2544f78bfe7513d322f0f87e010a5
Ant <it@me.org>
2022-01-30 22:43:58 -0600

    Change all workouts handlers to return HttpHandler

--- PiggCrapp.Api/WorkoutsHandlers.fs
+++ PiggCrapp.Api/WorkoutsHandlers.fs
@@ -28,59 +28,57 @@ type putWorkoutDto =
     { Variation : string
       Date : string }
 
-let getWorkoutsHandler userId next ctx = task {
-    let! workouts = findWorkoutsAsync <| UserId userId
-    let dtos =
-        workouts
-        |> List.map getWorkoutDto.fromDomain
-    return! json dtos next ctx
-}
+let getWorkoutsHandler userId : HttpHandler =
+    fun next ctx -> task {
+        let! workouts = findWorkoutsAsync (UserId userId)
+        let dtos =
+            workouts
+            |> List.map getWorkoutDto.fromDomain
+        return! json dtos next ctx
+    }
 
-let getWorkoutHandler workoutId next ctx = task {
-    match! findWorkoutAsync workoutId with
-    | Some workout ->
-        let dto = getWorkoutDto.fromDomain workout
-        return! json dto next ctx
-    | None ->
-        return! RequestErrors.NOT_FOUND {| message = "No workout with Id found." |} next ctx
-}
+let getWorkoutHandler workoutId : HttpHandler =
+    fun next ctx -> task {
+        match! findWorkoutAsync workoutId with
+        | Some workout ->
+            let dto = getWorkoutDto.fromDomain workout
+            return! json dto next ctx
+        | None ->
+            return! RequestErrors.NOT_FOUND {| message = "No workout with Id found." |} next ctx
+    }
 
-let postWorkoutHandler userId next (ctx:HttpContext) = task {
-    let! dto = ctx.BindJsonAsync<postWorkoutDto> ()
-    let variation = WorkoutVariation.fromString dto.Variation
-    let workout = Workout.create variation (UserId userId)
-    do! insertWorkoutAsync workout |> Task.ignore
-    let guid = WorkoutId.toGuid workout.WorkoutId
-    return! redirectTo false $"https://localhost:7162/workouts/{guid}" next ctx
-}
+let postWorkoutHandler userId : HttpHandler =
+    fun next ctx -> task {
+        let! dto = ctx.BindJsonAsync<postWorkoutDto> ()
+        let variation = WorkoutVariation.fromString dto.Variation
+        let workout = Workout.create variation (UserId userId)
+        do! insertWorkoutAsync workout |> Task.ignore
+        let guid = WorkoutId.toGuid workout.WorkoutId
+        return! redirectTo false $"https://localhost:7162/workouts/{guid}" next ctx
+    }
 
-let updateWorkoutHandler workoutId next (ctx:HttpContext) = task {
-    let! workout =
-        workoutId
-        |> findWorkoutAsync
-        |> Task.map List.tryHead
-    match workout with
-    | Some w ->
-        let! dto = ctx.BindJsonAsync<putWorkoutDto> ()
-        let date = DateTime.Parse(dto.Date)
-        let workout =
-            { w with
-                Variation = WorkoutVariation.fromString dto.Variation
-                Date = date }
-        updateWorkoutAsync workout |> Async.AwaitTask |> Async.RunSynchronously |> ignore
-        let response = getWorkoutDto.fromDomain workout
-        return! Successful.CREATED response next ctx
-    | None ->
-        return! RequestErrors.NOT_FOUND {| message = "No workout found with that Id"|} next ctx
-}
+let updateWorkoutHandler workoutId : HttpHandler =
+    fun next ctx -> task {
+        match! findWorkoutAsync workoutId with
+        | Some w ->
+            let! dto = ctx.BindJsonAsync<putWorkoutDto> ()
+            let date = DateTime.Parse(dto.Date)
+            let workout =
+                { w with
+                    Variation = WorkoutVariation.fromString dto.Variation
+                    Date = date }
+            updateWorkoutAsync workout |> Async.AwaitTask |> Async.RunSynchronously |> ignore
+            let response = getWorkoutDto.fromDomain workout
+            return! Successful.CREATED response next ctx
+        | None ->
+            return! RequestErrors.NOT_FOUND {| message = "No workout found with that Id"|} next ctx
+    }
 
-let deleteWorkoutHandler workoutId next (ctx:HttpContext) = task {
-    let! res = deleteWorkoutAsync <| WorkoutId workoutId
-    if
-        res = 1
-    then
-        ctx.SetStatusCode 204
-        return! json {||} next ctx
-    else
-        return! RequestErrors.NOT_FOUND {| message = "No workout found with that Id" |}next ctx
-}
+let deleteWorkoutHandler workoutId : HttpHandler =
+    fun next ctx -> task {
+        match! deleteWorkoutAsync (WorkoutId workoutId) with
+        | 1 ->
+            return! Successful.NO_CONTENT next ctx
+        | _ ->
+            return! RequestErrors.NOT_FOUND {| message = "No workout found with that Id" |} next ctx
+    }
