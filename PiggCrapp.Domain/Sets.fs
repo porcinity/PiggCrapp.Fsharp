b60a8ed232edc8abc19a1d3401202ef4c50496ac
Ant <it@me.org>
2022-01-31 15:40:23 -0600

    Auto format Sets.fs

--- PiggCrapp.Domain/Sets.fs
+++ PiggCrapp.Domain/Sets.fs
@@ -11,13 +11,13 @@ module Weight =
         | x when x > 1000.00 -> TooHeavy
         | x when x < 1.00 -> TooLight
         | _ -> GoodWeight
-    
+
     let fromDbl n =
         match n with
         | TooHeavy -> Error [ "Too heavy." ]
         | TooLight -> Error [ "Too light." ]
         | GoodWeight -> Weight n |> Ok
-        
+
     let toDouble (Weight n) = n
 
 type Reps = Reps of int
@@ -28,26 +28,26 @@ module Reps =
         | x when x > 100 -> TooMany
         | x when x < 0 -> TooFew
         | _ -> GoodReps
-        
+
     let fromInt n =
         match n with
         | TooMany -> Error [ "Too many reps." ]
         | TooFew -> Error [ "Too few reps." ]
         | GoodReps -> Reps n |> Ok
-        
+
     let toInt (Reps n) = n
 
 type RegularSetId = RegularSetId of int
 
 module RegularSetId =
     let toInt (RegularSetId id) = id
-    
+
 type RegularSet =
-    { RegularSetId : RegularSetId 
-      Weight : Weight
-      Reps : Reps
-      Exercise : ExerciseId }
-    
+    { RegularSetId: RegularSetId
+      Weight: Weight
+      Reps: Reps
+      Exercise: ExerciseId }
+
 module RegularSet =
     let create =
         fun exerciseId setId weight reps ->
@@ -59,42 +59,44 @@ module RegularSet =
     let update =
         fun set updatedSet ->
             { set with
-                RegularSetId = updatedSet.RegularSetId
-                Weight = updatedSet.Weight
-                Reps = updatedSet.Reps }
-    
+                  RegularSetId = updatedSet.RegularSetId
+                  Weight = updatedSet.Weight
+                  Reps = updatedSet.Reps }
+
 type RpSetId = RpSetId of Guid
-    
+
 type RestPauseSet =
-    { RpSetId : RpSetId
-      Weight : Weight
-      RestPauseSets : Reps list
-      Exercise : ExerciseId }
-    
+    { RpSetId: RpSetId
+      Weight: Weight
+      RestPauseSets: Reps list
+      Exercise: ExerciseId }
+
 module RestPauseSet =
     let create =
         fun weight exerciseId ->
-            { RpSetId = RpSetId <| Guid.NewGuid() 
+            { RpSetId = RpSetId <| Guid.NewGuid()
               Weight = weight
               RestPauseSets = []
               Exercise = exerciseId }
-    
+
     let addReps rpSet reps =
-        { rpSet with RestPauseSets = rpSet.RestPauseSets @ [reps] }
-    
+        { rpSet with
+              RestPauseSets = rpSet.RestPauseSets @ [ reps ] }
+
     let removeReps rpSet reps =
         let setMinus =
             rpSet.RestPauseSets
             |> List.filter (fun r -> r <> reps)
+
         { rpSet with RestPauseSets = setMinus }
-    
+
 type DropSetId = DropSetId of Guid
-    
+
 type DropSet =
-    { DropSetId : DropSetId
-      DropSets : RegularSet list
-      Exercise : ExerciseId }
-    
+    { DropSetId: DropSetId
+      DropSets: RegularSet list
+      Exercise: ExerciseId }
+
 type Set =
     | Regular of RegularSet
     | RestPause of RestPauseSet
