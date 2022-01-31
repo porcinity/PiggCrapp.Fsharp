5318b063ec3eeee6a0780a44e98f86df1c264ee0
Ant <it@me.org>
2022-01-31 15:25:24 -0600

    Auto format Program.s

--- PiggCrapp.Api/Program.fs
+++ PiggCrapp.Api/Program.fs
@@ -12,65 +12,58 @@ open PiggCrapp.Api.ExercisesHandlers
 open PiggCrapp.Api.SetsHandlers
 open Giraffe
 
-let errorHandler (ex : Exception) (logger : ILogger) =
+let errorHandler (ex: Exception) (logger: ILogger) =
     logger.LogError(EventId(), ex, "An unhandled exception has occurred while executing the request.")
+
     clearResponse
     >=> ServerErrors.INTERNAL_ERROR ex.Message
 
 let webApp =
-    choose [
-        GET >=>
-            choose [
-                route "/users" >=> getUsersHandler
-                routef "/users/%s" getUserHandler
-                routef "/users/%O/workouts" getWorkoutsHandler
-                routef "/workouts/%O" getWorkoutHandler
-                routef "/workouts/%O/exercises" getExercisesHandler
-                routef "/exercises/%O" getExerciseHandler
-                routef "/exercises/%O/sets" getSetsHandler
-                routef "/exercises/%O/sets/%i" getSetHandler
-            ]
-        POST >=>
-            choose [
-                route "/users" >=> postUserHandler
-                routef "/users/%O/workouts" postWorkoutHandler
-                routef "/workouts/%O/exercises" postExerciseHandler
-                routef "/exercises/%O/sets" postSetHandler
-            ]
-        PUT >=>
-            choose [
-                routef "/users/%O" updateUserHandler
-                routef "/workouts/%O" updateWorkoutHandler
-                routef "/exercises/%O" updateExerciseHandler
-                routef "/exercises/%O/sets/%i" updateSetHandler
-            ]
-        DELETE >=>
-            choose [
-                routef "/users/%O" deleteUserHandler
-                routef "/workouts/%O" deleteWorkoutHandler
-                routef "/exercises/%O" deleteExerciseHandler
-                routef "/exercises/%O/sets/%i" deleteSetHandler
-            ]
-        ]
+    choose [ GET
+             >=> choose [ route "/users" >=> getUsersHandler
+                          routef "/users/%s" getUserHandler
+                          routef "/users/%O/workouts" getWorkoutsHandler
+                          routef "/workouts/%O" getWorkoutHandler
+                          routef "/workouts/%O/exercises" getExercisesHandler
+                          routef "/exercises/%O" getExerciseHandler
+                          routef "/exercises/%O/sets" getSetsHandler
+                          routef "/exercises/%O/sets/%i" getSetHandler ]
+             POST
+             >=> choose [ route "/users" >=> postUserHandler
+                          routef "/users/%O/workouts" postWorkoutHandler
+                          routef "/workouts/%O/exercises" postExerciseHandler
+                          routef "/exercises/%O/sets" postSetHandler ]
+             PUT
+             >=> choose [ routef "/users/%O" updateUserHandler
+                          routef "/workouts/%O" updateWorkoutHandler
+                          routef "/exercises/%O" updateExerciseHandler
+                          routef "/exercises/%O/sets/%i" updateSetHandler ]
+             DELETE
+             >=> choose [ routef "/users/%O" deleteUserHandler
+                          routef "/workouts/%O" deleteWorkoutHandler
+                          routef "/exercises/%O" deleteExerciseHandler
+                          routef "/exercises/%O/sets/%i" deleteSetHandler ] ]
 
-let configureApp (app : IApplicationBuilder) =
+let configureApp (app: IApplicationBuilder) =
     // Add Giraffe to the ASP.NET Core pipeline
-    app.UseGiraffeErrorHandler(errorHandler)
-       .UseGiraffe webApp
+    app
+        .UseGiraffeErrorHandler(errorHandler)
+        .UseGiraffe webApp
 
-let configureServices (services : IServiceCollection) =
+let configureServices (services: IServiceCollection) =
     // Add Giraffe dependencies
     services.AddGiraffe() |> ignore
 
 [<EntryPoint>]
 let main _ =
-    Host.CreateDefaultBuilder()
-        .ConfigureWebHostDefaults(
-            fun webHostBuilder ->
-                webHostBuilder
-                    .Configure(configureApp)
-                    .ConfigureServices(configureServices)
-                    |> ignore)
+    Host
+        .CreateDefaultBuilder()
+        .ConfigureWebHostDefaults(fun webHostBuilder ->
+            webHostBuilder
+                .Configure(configureApp)
+                .ConfigureServices(configureServices)
+            |> ignore)
         .Build()
         .Run()
+
     0
