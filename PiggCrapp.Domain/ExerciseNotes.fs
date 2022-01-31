3cfead51c6af21d2d9a83dffdf2e2084c63baa5c
Ant <it@me.org>
2022-01-31 15:39:28 -0600

    Auto format ExerciseNotes.fs

--- PiggCrapp.Domain/ExerciseNotes.fs
+++ PiggCrapp.Domain/ExerciseNotes.fs
@@ -1,27 +1,25 @@
 module PiggCrapp.Domain.ExerciseNotes
 
-type SeatHeight =
-    | SeatHeight of int
-    
-type SeatPosition =
-    | SeatPosition of int
-    
+type SeatHeight = SeatHeight of int
+
+type SeatPosition = SeatPosition of int
+
 type SeatSetting =
     | SeatHeight
     | SeatPosition
     | MultiSetting of SeatHeight * SeatPosition
-    
+
 type Manufacturer =
     | Hoist
     | Nautilus
-    
-type PlateLoaded = PlateLoaded 
-    
+
+type PlateLoaded = PlateLoaded
+
 type MachineType =
     | PlateLoaded
     | Selectorized of Manufacturer
     | CableStack
-    
+
 type ExerciseNotes =
-    { SeatSetting : SeatSetting
-      MachineType : MachineType }
+    { SeatSetting: SeatSetting
+      MachineType: MachineType }
